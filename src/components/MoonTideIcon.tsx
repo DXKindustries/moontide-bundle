710bc3f026ded188596a0f7c214ba222bac77fa6
DXKindustries <dxkindustries@yahoo.com>
2025-07-17 14:44:56 +0000

    Update MoonTideIcon to new SVG artwork

--- src/components/MoonTideIcon.tsx
+++ src/components/MoonTideIcon.tsx
@@ -4,16 +4,24 @@ export default function MoonTideIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
-      strokeWidth={2}
-      strokeLinecap="round"
-      strokeLinejoin="round"
       {...props}
     >
-      <circle cx="12" cy="8" r="5" />
-      <path d="M2 16c2-2 6-2 8 0s6 2 8 0" />
+      <g>
+        <circle cx="12" cy="12" r="10" fill="#23395d" />
+        <path
+          d="M16.8 15.5c-1.2.7-2.6 1.1-4.1 1.1-3.6 0-6.6-2.2-6.6-5 0-1.5 1.1-2.8 2.7-3.6"
+          fill="#f4d35e"
+        />
+        <path
+          d="M8.6 8.4a5.5 5.5 0 0 1 9 3.6c0 2.8-3 5-6.6 5-1.5 0-2.9-.4-4.1-1.1"
+          fill="#fff"
+        />
+        <circle cx="15" cy="12" r="1" fill="#23395d" />
+      </g>
     </svg>
   );
 }
