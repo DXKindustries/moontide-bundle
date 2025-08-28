e3ad391e578665f59d25b9fba3f2aae5dc3e540f
DXKindustries <dxkindustries@yahoo.com>
2025-08-28 15:56:57 -0400

    Remove leftover Capacitor references

--- android/app/src/main/java/com/example/moontide/MainActivity.java
+++ android/app/src/main/java/com/example/moontide/MainActivity.java
@@ -1,5 +1,13 @@
 package com.example.moontide;
 
-import com.getcapacitor.BridgeActivity;
+import android.app.Activity;
+import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
